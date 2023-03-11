64bbc8fdfe740791e0b2c448329a7a7eb8140f71
Mykhailo Friziuk <ibetop1795.mf@gmail.com>
2023-03-11 16:19:11 +0200

    change

---
+++ js/train.js
@@ -0,0 +1,12 @@
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Change code below this line
+  pricePerDroid: 3000, 1000, 5000, 2000, 500;
+  orderedQuantity: 5, 3, 10, 8, 10;
+  customerCredits: 23000, 15000, 8000, 10000, 5000;
+  message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - pricePerDroid} credits left`;
+let totalPrice; 
+  console.log()
+  // Change code above this line
+  return message;
+}
