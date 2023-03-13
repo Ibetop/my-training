b59aad6d8aae747788fe22e5331bd0a0a1ae921b
Mykhailo Friziuk <ibetop1795.mf@gmail.com>
2023-03-13 20:52:36 +0200

    change

--- js/train.js
+++ js/train.js
@@ -1,12 +1,22 @@
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-  // Change code below this line
-  pricePerDroid: 3000, 1000, 5000, 2000, 500;
-  orderedQuantity: 5, 3, 10, 8, 10;
-  customerCredits: 23000, 15000, 8000, 10000, 5000;
-  message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - pricePerDroid} credits left`;
-let totalPrice; 
-  console.log()
-  // Change code above this line
-  return message;
-}
+// let misha = ["Misha", "Taras", "Kostya", "Olya", "Andrey", "Stas", "Tatyana", "Polya"];
+// for ( let i = 0; i < misha.length; i += 1){
+// console.log(misha[i])
+// }
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+// const misha = "mykhailo";
+
+// for (const mikel of misha) {
+//   console.log(mikel);
+// }
+const message = "JavaScript - це цікаво";
+console.log(message.split("")); 
