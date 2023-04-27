e5cf02e7b988a63bb4f705ee83678e32f43d8634
Mykhailo Friziuk <ibetop1795.mf@gmail.com>
2023-04-27 16:17:40 +0300

    change

--- js/train.js
+++ js/train.js
@@ -144,17 +144,4 @@
 //     updateGenre(newGenre) {
 //       this.genre = newGenre;
 //     },
-// };
-const forecast = {
-    today: {
-      low: 28,
-      high: 32,
-      icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
-    },
-    tomorrow: {
-      low: 27,
-      high: 31,
-    },
-  };
-  // Change code below this line
-  const today: {low: highToday, high: highToday, icon: todayIcon:"https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" },  tomorrow: {low: highTomorrow, high: highTomorrow, icon: tomorrowIcon:"https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = forecast;
+
