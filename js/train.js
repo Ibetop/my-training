cb9f586bd70e341d55343a1d1d0255515320a8be
Mykhailo Friziuk <ibetop1795.mf@gmail.com>
2023-05-12 17:32:42 +0300

    change

--- js/train.js
+++ js/train.js
@@ -208,14 +208,21 @@
 
 // const arrayM = m.name;
 // console.log(arrayM)
-function countProps(object) {
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
   // Change code below this line
-  let propCount = 0;
+for (const product of productName) {
+  console.log(product)
+  
+}
 
-  const keys = Object.keys(object);
-  propCount = keys.length
 
-  return propCount;
   // Change code above this line
 }
-console.log(countProps({ name: "Mango", age: 2, old: 3 }))
+console.log(getProductPrice(productName({ name: "Radar", price: 1300, quantity: 4 })))
