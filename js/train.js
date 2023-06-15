d48475030501e92eb19817ed537d12e9fa41789c
Ibetop <ibetop1795.mf@gmail.com>
2023-06-15 12:29:41 +0100

    change

--- js/train.js
+++ js/train.js
@@ -232,3 +232,43 @@
 //   // Change code above this line
 // };
 // console.log(`bookShelf.updateBook('Sands of dune', 'Dune')`);
+const atTheOldToad = {
+    potions: [
+      { name: "Speed potion", price: 460 },
+      { name: "Dragon breath", price: 780 },
+      { name: "Stone skin", price: 520 },
+    ],
+    // Change code below this line
+    getPotions() {
+      return this.potions;
+    },
+    addPotion(newPotion) {
+      if (newPotion === this.potions ) {
+        return `Error! Potion ${newPotion} is already in your inventory!`;
+      }
+  
+      this.potions.push(newPotion);
+    },
+    removePotion(potionName) {
+      const potionIndex = this.potions.indexOf(potionName);
+  
+      if (potionIndex === potionName) {
+        return `Potion ${potionName} is not in inventory!`;
+      }
+  
+      this.potions.splice(potionIndex, 1);
+    },
+    updatePotionName(oldName, newName) {
+      const potionIndex = this.potions.indexOf(oldName);
+      console.log(potionIndex);
+     
+      if (potionIndex === newName) {
+        return `Potion ${oldName} is not in inventory!`;
+      }
+  
+      this.potions.splice(potionIndex, 1, newName);
+    },
+    // Change code above this line
+  };
+
+console.log(atTheOldToad.updatePotionName('Stone skin', 'Invulnerability potion'));
